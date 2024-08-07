84bc286d702948c5b2e9f0a0bbcd7af2c9072099
Savita <savitaranipathak@hotmail.com>
2024-08-07 12:26:48 -0400

    new items added

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -4,10 +4,31 @@ import './EventPlanner.css'; // Import CSS file for styling
 const EventPlanner = () => {
     return (
         <div className="event-planner-container">
-            <header>
-                <h1>Welcome to Event Planner</h1>
-            </header>
-        </div>
+        <header>
+            <h1>Welcome to Event Planner</h1>
+        </header>
+        <section className="description">
+                <p>
+                    Plan and organize your events effortlessly with Event Planner. From
+                    birthdays to corporate meetings, we've got you covered.
+                </p>
+
+
+                <button className="get-started-button">Get Started</button>
+            </section>
+        <section className="events_categories">
+
+        </section>
+        <section className="features">
+
+        </section>
+        <section className="testimonials">
+
+        </section>
+        <section className="contact">
+
+        </section>
+    </div>
     );
 };
 
